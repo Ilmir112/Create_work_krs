8ce29a9ae949cf676bd9388bf651e2f4bf4ef6cf
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2024-10-08 16:00:17 +0500

    Скорректировал расчет

--- data_base/config_base.py
+++ data_base/config_base.py
@@ -6,6 +6,8 @@ import psycopg2
 
 
 # Загрузите переменные окружения из файла .env
+import well_data
+
 load_dotenv()
 
 
@@ -16,14 +18,16 @@ def connect_to_database(DB_NAME):
     # Определяем путь к файлу .env
     if getattr(sys, 'frozen', False):  # Проверка, запущен ли скрипт как исполняемый файл
         print('1')
-        env_path = os.path.join(sys._MEIPASS, '.env')  # Получаем путь к .env
+        env_path = os.path.join(sys._MEIPASS, f'{well_data.path_image}.env')
+
     else:
         env_path = '.env'
-        load_dotenv(env_path)
-        DB_USER = os.getenv('DB_USER')
-        DB_PASSWORD = os.getenv('DB_PASSWORD')
-        DB_HOST = os.getenv('DB_HOST')
-        DB_PORT = os.getenv('DB_PORT')
+        load_dotenv(env_path)  # Получаем путь к .env
+
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
 
         # Если не исполняемый файл, используем текущую директорию
     print(env_path)
