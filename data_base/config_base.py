f258b5e626be0e72124a8d8525abb61d4d28fd02
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2024-10-08 18:10:06 +0500

    Скорректировал расчет

--- data_base/config_base.py
+++ data_base/config_base.py
@@ -16,22 +16,18 @@ load_dotenv()
 # Функция подключения к базе данных
 def connect_to_database(DB_NAME):
     # Определяем путь к файлу .env
-    if getattr(sys, 'frozen', False):  # Проверка, запущен ли скрипт как исполняемый файл
+    # if getattr(sys, 'frozen', False):  # Проверка, запущен ли скрипт как исполняемый файл
+    #
+    #     env_path = os.path.join(sys._MEIPASS, '.env')
+    # else:
+    #     env_path = '.env'
 
-        env_path = os.path.join(sys._MEIPASS, '.env')
-    else:
-        env_path = '.env'
-
-    load_dotenv(env_path)  # Получаем путь к .env
+    load_dotenv()  # Получаем путь к .env
 
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = os.getenv('DB_HOST')
     DB_PORT = os.getenv('DB_PORT')
-
-        # Если не исполняемый файл, используем текущую директорию
-    print(env_path)
-
 
     try:
 
