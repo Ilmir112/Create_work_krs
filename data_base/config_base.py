dc0fb9c432a23215b8784c573fb7a3e34a8d830e
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2024-10-08 14:43:53 +0500

    Скорректировал расчет

--- data_base/config_base.py
+++ data_base/config_base.py
@@ -8,15 +8,7 @@ import psycopg2
 # Загрузите переменные окружения из файла .env
 load_dotenv()
 
-# Получите настройки подключения из переменных окружения
-DB_WELL_DATA = os.getenv('DB_WELL_DATA')
-DB_NAME_USER = os.getenv('DB_NAME_USER')
-DB_USER = os.getenv('DB_USER')
-DB_NAME_GNKT = os.getenv('DB_NAME_GNKT')
-DB_CLASSIFICATION = os.getenv('DB_CLASSIFICATION')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+
 
 
 # Функция подключения к базе данных
@@ -26,15 +18,19 @@ def connect_to_database(DB_NAME):
         print('1')
         env_path = os.path.join(sys._MEIPASS, '.env')  # Получаем путь к .env
     else:
-        env_path = '.env'  # Если не исполняемый файл, используем текущую директорию
+        env_path = '.env'
+        load_dotenv(env_path)
+        DB_USER = os.getenv('DB_USER')
+        DB_PASSWORD = os.getenv('DB_PASSWORD')
+        DB_HOST = os.getenv('DB_HOST')
+        DB_PORT = os.getenv('DB_PORT')
+
+        # Если не исполняемый файл, используем текущую директорию
     print(env_path)
     # Загрузите переменные окружения из файла .env
-    load_dotenv(env_path)
 
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
+
+
     print(DB_USER)
 
     try:
