c600e6e28c227d96c80b68fff4f63072802c06c7
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2024-10-09 09:10:56 +0500

    Скорректировал расчет

--- data_base/config_base.py
+++ data_base/config_base.py
@@ -4,25 +4,17 @@ import sys
 from dotenv import load_dotenv
 import psycopg2
 
+load_dotenv()  # Получаем путь к .env
+print("DB_USER:", os.getenv('DB_USER'))
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
 
 # Функция подключения к базе данных
 def connect_to_database(DB_NAME):
-    # Определяем путь к файлу .env
-    # if getattr(sys, 'frozen', False):  # Проверка, запущен ли скрипт как исполняемый файл
-    #
-    #     env_path = os.path.join(sys._MEIPASS, '.env')
-    # else:
-    #     env_path = '.env'
-
-    load_dotenv()  # Получаем путь к .env
-    print("DB_USER:", os.getenv('DB_USER'))
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT')
 
     try:
-
         connection = psycopg2.connect(
             dbname=DB_NAME,
             user=DB_USER,
