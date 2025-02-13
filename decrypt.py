b25ed7bc313319fd4b0d6adb3dd108fa516bba1c
Ilmir112 <137054729+Ilmir112@users.noreply.github.com>
2025-02-13 15:08:51 +0500

    Добавил копирование, вставка

--- decrypt.py
+++ decrypt.py
@@ -1,17 +1,23 @@
+import sys
+
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 import os
 import data_list
 
 def decrypt(name):
-    print(f"{data_list.path_image}key.key")
+
     # Загрузка ключа
     with open(f"{data_list.path_image}key.key", 'rb') as key_file:
         key = key_file.read()
     cipher = Fernet(key)
 
+    ext_data_dir = os.getcwd()
+    if getattr(sys, 'frozen', False):
+        ext_data_dir = sys._MEIPASS
+
     # Загружаем .env файл
-    load_dotenv()
+    load_dotenv(dotenv_path=os.path.join(ext_data_dir, '.env'))
     encrypted_value = os.getenv(name).encode()
 
     # Дешифруем значение
