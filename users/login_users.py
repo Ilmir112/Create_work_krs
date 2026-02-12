c649e822dd66146520f4d00894754268c29f8bb0
ilmir112 <zufaroff_112@gmail.com>
2026-02-12 12:26:40 +0500

    мелкие исправления

--- users/login_users.py
+++ users/login_users.py
@@ -86,7 +86,6 @@ class LoginWindow(QDialog):
             user_access = ApiClient.get_info_data(params, ApiClient.login_path())
             if user_access and user_access != 401:
                 ApiClient.SETTINGS_TOKEN = QSettings('Zima', 'ZimaApp')
-
                 ApiClient.SETTINGS_TOKEN.setValue('auth_token', user_access["access_token"])
 
                 self.user_dict = user_access
